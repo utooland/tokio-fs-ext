0431feae3610217e1873cb955c22d35d5fed61a2
xusd320 <xusd320@gmail.com>
2025-07-28 14:14:05 +0800

    feat: add DirBuilder

---
+++ src/fs/wasm/dir_builder.rs
@@ -0,0 +1,28 @@
+use std::{io, path::Path};
+
+use crate::fs::{create_dir, create_dir_all};
+
+#[derive(Debug, Default)]
+pub struct DirBuilder {
+    recursive: bool,
+}
+
+impl DirBuilder {
+    pub fn new() -> Self {
+        DirBuilder::default()
+    }
+
+    pub fn recursive(&mut self, recursive: bool) -> &mut Self {
+        self.recursive = recursive;
+        self
+    }
+
+    pub async fn create(&self, path: impl AsRef<Path>) -> io::Result<()> {
+        if self.recursive {
+            create_dir_all(path).await?;
+        } else {
+            create_dir(path).await?;
+        }
+        Ok(())
+    }
+}
