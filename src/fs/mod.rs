b3d301b2670bc4692c56e3f2fc2b1de845eb5daf
xusd320 <xusd320@gmail.com>
2025-07-28 23:47:33 +0800

    fix: remove nightly feature directive

--- src/fs/mod.rs
+++ src/fs/mod.rs
@@ -1,53 +1,22 @@
-cfg_select! {
-    all(target_family = "wasm", target_os = "unknown") => {
-        mod wasm;
-        pub(crate) use wasm::opfs;
-        pub use wasm::{
-            canonicalize,
-            copy,
-            create_dir,
-            create_dir_all,
-            DirBuilder,
-            File,
-            metadata,
-            OpenOptions,
-            read, DirEntry, ReadDir,
-            read_dir,
-            read_link,
-            read_to_string,
-            remove_dir,
-            remove_dir_all,
-            remove_file,
-            rename,
-            symlink,
-            try_exists,
-            write,
+#[cfg(all(target_family = "wasm", target_os = "unknown"))]
+mod wasm;
 
-        };
-    }
-    not(all(target_family = "wasm", target_os = "unknown")) => {
-        pub use tokio::fs::{
-            canonicalize,
-            copy,
-            create_dir,
-            create_dir_all,
-            DirBuilder,
-            File,
-            metadata,
-            OpenOptions,
-            read, DirEntry, ReadDir,
-            read_dir,
-            read_link,
-            read_to_string,
-            remove_dir,
-            remove_dir_all,
-            remove_file,
-            rename,
-            try_exists,
-            write,
-        };
-    }
-    target_family = "unix" => {
-        pub use tokio::fs::symlink;
-    }
-}
+#[cfg(all(target_family = "wasm", target_os = "unknown"))]
+pub(crate) use wasm::opfs;
+
+#[cfg(all(target_family = "wasm", target_os = "unknown"))]
+pub use wasm::{
+    DirBuilder, DirEntry, File, OpenOptions, ReadDir, canonicalize, copy, create_dir,
+    create_dir_all, metadata, read, read_dir, read_link, read_to_string, remove_dir,
+    remove_dir_all, remove_file, rename, symlink, try_exists, write,
+};
+
+#[cfg(any(target_family = "unix", target_family = "windows"))]
+pub use tokio::fs::{
+    DirBuilder, DirEntry, File, OpenOptions, ReadDir, canonicalize, copy, create_dir,
+    create_dir_all, metadata, read, read_dir, read_link, read_to_string, remove_dir,
+    remove_dir_all, remove_file, rename, try_exists, write,
+};
+
+#[cfg(target_family = "unix")]
+pub use tokio::fs::symlink;
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-#![feature(cfg_select)]
-
 pub mod fs;
 
 #[cfg(all(target_family = "wasm", target_os = "unknown"))]
