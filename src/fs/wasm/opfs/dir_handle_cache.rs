eb91655619e231d0c8c96a7ce8c881578a523e16
xusd320 <xusd320@gmail.com>
2025-08-19 14:06:53 +0800

    refactor: remove LazyLock in dir_handle_cache

--- src/fs/wasm/opfs/dir_handle_cache.rs
+++ src/fs/wasm/opfs/dir_handle_cache.rs
@@ -1,40 +1,40 @@
-use std::{path::PathBuf, sync::LazyLock};
+use std::{cell::RefCell, path::PathBuf};
 
 use rustc_hash::FxHashMap;
 use web_sys::FileSystemDirectoryHandle;
 
-static mut DIR_HANDLE_CACHE: LazyLock<FxHashMap<PathBuf, FileSystemDirectoryHandle>> =
-    LazyLock::new(FxHashMap::default);
+thread_local! {
+    static  DIR_HANDLE_CACHE: RefCell<FxHashMap<PathBuf, FileSystemDirectoryHandle>> = RefCell::new(FxHashMap::default());
+}
 
 pub(super) fn get_cached_dir_handle(path: &PathBuf) -> Option<FileSystemDirectoryHandle> {
-    unsafe {
-        #[allow(static_mut_refs)]
-        DIR_HANDLE_CACHE.get(path).cloned()
-    }
+    DIR_HANDLE_CACHE.with(|cache| cache.borrow().get(path).cloned())
 }
 
 pub(super) fn set_cached_dir_handle(path: PathBuf, handle: FileSystemDirectoryHandle) {
-    unsafe {
-        #[allow(static_mut_refs)]
-        DIR_HANDLE_CACHE.insert(path.clone(), handle);
-    }
+    DIR_HANDLE_CACHE.with(|cache| {
+        cache.borrow_mut().insert(path, handle);
+    })
 }
 
 pub(super) fn remove_cached_dir_handle(path: &PathBuf, recursive: bool) {
-    if recursive {
-        unsafe {
-            #[allow(static_mut_refs)]
-            DIR_HANDLE_CACHE
+    DIR_HANDLE_CACHE.with(|cache| {
+        let keys: Vec<PathBuf> = {
+            cache
+                .borrow()
                 .keys()
                 .filter(|k| k.starts_with(path))
-                .for_each(|k| {
-                    DIR_HANDLE_CACHE.remove(k);
-                });
-        }
-    } else {
-        unsafe {
-            #[allow(static_mut_refs)]
-            DIR_HANDLE_CACHE.remove(path)
+                .cloned()
+                .collect()
         };
-    }
+
+        let mut cache = cache.borrow_mut();
+        if recursive {
+            keys.into_iter().for_each(|k| {
+                cache.remove(&k);
+            });
+        } else {
+            cache.remove(path);
+        }
+    })
 }
