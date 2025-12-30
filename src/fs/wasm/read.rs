1ec4b4a84f1225ee3081e44173b4568bca905d98
xusd320 <xusd320@gmail.com>
2025-12-30 16:30:21 +0800

    perf(wasm): remove size hint in fs::read

--- src/fs/wasm/read.rs
+++ src/fs/wasm/read.rs
@@ -5,9 +5,7 @@ use super::OpenOptions;
 pub async fn read(path: impl AsRef<Path>) -> io::Result<Vec<u8>> {
     let mut file = OpenOptions::new().read(true).open(path).await?;
 
-    let file_size = file.size()?;
-
-    let mut buf = vec![0; file_size as usize];
+    let mut buf = vec![];
 
     file.read_to_buf(&mut buf)?;
 
