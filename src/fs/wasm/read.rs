e1198d4c81794ea72e44cf9deb81a53a802b8ecc
xusd320 <xusd320@gmail.com>
2025-12-30 17:42:19 +0800

    feat: use file size as capacity hint in wasm read

--- src/fs/wasm/read.rs
+++ src/fs/wasm/read.rs
@@ -1,15 +1,15 @@
 use std::{io, path::Path};
 
-use futures::io::AsyncReadExt;
-
 use super::OpenOptions;
 
 pub async fn read(path: impl AsRef<Path>) -> io::Result<Vec<u8>> {
     let mut file = OpenOptions::new().read(true).open(path).await?;
 
-    let mut buf = vec![];
+    let file_size = file.size()?;
 
-    file.read_to_end(&mut buf).await?;
+    let mut buf = vec![0; file_size as usize];
+
+    file.read_to_buf(&mut buf)?;
 
     Ok(buf)
 }
