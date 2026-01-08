cb1471ca651ffce3c9648b358cf307474512f7c8
xusd320 <xusd320@gmail.com>
2026-01-08 23:08:38 +0800

    fix(wasm): seek when write

--- src/fs/wasm/write.rs
+++ src/fs/wasm/write.rs
@@ -1,7 +1,5 @@
 use std::{io, path::Path};
 
-use futures::io::AsyncSeekExt;
-
 use super::OpenOptions;
 
 pub async fn write(path: impl AsRef<Path>, content: impl AsRef<[u8]>) -> io::Result<()> {
@@ -11,8 +9,6 @@ pub async fn write(path: impl AsRef<Path>, content: impl AsRef<[u8]>) -> io::Res
         .write(true)
         .open(path)
         .await?;
-
-    file.seek(io::SeekFrom::Start(0)).await?;
 
     file.write_with_buf(content.as_ref())?;
 
